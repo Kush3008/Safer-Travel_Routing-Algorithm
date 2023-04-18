0da11b6a597a633a56c836245fea901a9f8d10a8
Kushagra <83759659+Kush3008@users.noreply.github.com>
2023-04-18 15:41:39 +0530

    Add files via upload

---
+++ Path Ml/pythonScript/kmeans.py
@@ -0,0 +1,52 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv('C:\\Users\\akush\\Desktop\\Safer Travels Final Run\\Code\\Path Ml algo\\data\\crime.csv')
+X = dataset.iloc[:, [1,2,3,4,5,6,7,12]].values
+
+
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+
+
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 21):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 21), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+kmeans = KMeans(n_clusters = 6, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+
+from sklearn.decomposition import KernelPCA
+kpca = KernelPCA(n_components = 2, kernel = 'rbf')
+X = kpca.fit_transform(X)
+
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta' )
+plt.scatter(X[y_kmeans == 5, 0], X[y_kmeans == 5, 1], s = 100, c = 'black' )
+
+
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of areas')
+plt.xlabel('factor1')
+plt.ylabel('factor2')
+plt.legend()
+plt.show()
